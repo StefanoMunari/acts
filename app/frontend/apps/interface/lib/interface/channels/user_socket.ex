c54c30d8bf45061693cde6874416a19e02140d25
snate <sebastiano.valle@hotmail.it>
2017-11-06 17:17:05 +0100

    Application Server: Clean from stale files and mocks
    Problem Solved
    The repository used to contain stale files or mocks developed during a prototyping period.
    
    Rational
    Chore: keep the repository clean

--- app/frontend/apps/interface/lib/interface/channels/user_socket.ex
+++ app/frontend/apps/interface/lib/interface/channels/user_socket.ex
@@ -2,7 +2,6 @@ defmodule Interface.UserSocket do
   use Phoenix.Socket
 
   channel "district:*", Interface.DistrictChannel
-  channel "traveller:*", Interface.TravellerChannel
 
   transport :websocket, Phoenix.Transports.WebSocket
   transport :longpoll, Phoenix.Transports.LongPoll
